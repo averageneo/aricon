89885298ab30af60adfea7cefe949b8ae6383205
Hossein Heydari <hossein7heydari@gmail.com>
2016-05-23 20:37:36 +0430

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-color='\033[0;36m'
-echo -e "\n ${color}------ This is Aricon Installer ------"
-sleep 2
-mkdir Aricon-files
-cp ./failed.ogg ffmpeg.png install.sh ./Aricon-files
-cp -R ./Aricon-files Aricon ~/.local/share/nautilus/scripts
-chmod +x ~/.local/share/nautilus/scripts/Aricon
